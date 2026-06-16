72a2bd02f8c14cbb6d5d80dd86a6b0d8873494d2
Admin <rs@DESKTOP-CJBULF8>
2026-06-15 22:08:41 -0500

    add fixed-width integer typedefs

---
+++ include/Vcpu/Types.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Vcpu
+{
+    typedef std::uint8_t UInt8;
+    typedef std::uint16_t UInt16;
+    typedef std::uint32_t UInt32;
+    typedef std::uint64_t UInt64;
+
+    typedef std::int8_t Int8;
+    typedef std::int16_t Int16;
+    typedef std::int32_t Int32;
+    typedef std::int64_t Int64;
+
+    typedef bool Boolean;
+
+    static const Boolean True = true;
+    static const Boolean False = false;
+}
